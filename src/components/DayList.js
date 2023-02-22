a7d208af6f66de73c3a2d8bfac7dc393d7655af5
andrii-grynik <andrii.grynik@gmail.com>
2023-02-21 19:59:24 -0700

    "DayLisy mapping"

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -2,6 +2,18 @@ import React from 'react';
 import DayListItem from 'components/DayListItem';
 
 
+// DayList renders a list of DayListItem components
 export default function DayList(props) {
-  return<ul></ul>
-}  
+  const schedule = props.days.map(dayObj => {
+ 
+    return (
+      <DayListItem
+        key={dayObj.id}
+        selected={dayObj.name === props.day}
+        setDay={props.setDay}
+        {...dayObj}
+      />
+    )
+  })
+  return <ul>{schedule}</ul>
+}
