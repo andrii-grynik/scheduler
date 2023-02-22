199db3532ad3b7192f98fb91a3c674ad8c2b3c8e
andrii-grynik <andrii.grynik@gmail.com>
2023-02-22 16:38:11 -0700

    "Empty addition"

--- stories/index.js
+++ stories/index.js
@@ -12,6 +12,8 @@ import InterviewerList from "components/InterviewerList";
 
 import Appointment from "components/Appointment/index.js";
 import Header from "components/Appointment/Header.js";
+import Empty from "components/Appointment/Empty.js";
+
 
 import Button from "components/Button";
 
@@ -113,3 +115,4 @@ storiesOf("Button", module)
     .add("Appointment", () => <Appointment />)
     .add("Appointment with Time", () => <Appointment time="12pm" />);
     .add("Header", () => <Header time="12pm" />)
+    .add("Empty", () => <Empty onAdd={action("onAdd")}/>)
