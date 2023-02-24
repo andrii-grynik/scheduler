4c66839ea4f39294427ab5d5d0c910b226431a65
andrii-grynik <andrii.grynik@gmail.com>
2023-02-23 18:13:32 -0700

    "requesting the days"

--- src/components/Application.js
+++ src/components/Application.js
@@ -57,7 +57,7 @@ export default function Application(props) {
       })
       .catch(err => console.log(err))
   }, [])
-  
+
   return (
     <main className="layout">
       <section className="sidebar">
@@ -77,7 +77,8 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {appointment}
+        <Appointment key="last" time="5pm" />
       </section>
     </main>
   );
