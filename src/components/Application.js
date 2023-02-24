98159be46596bf62d4c89930bf40eec7b7faba5d
andrii-grynik <andrii.grynik@gmail.com>
2023-02-23 18:11:34 -0700

    "App.js update"

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,48 +1,16 @@
 import React, { Fragment, useState, useEffect } from "react";
-import DayList from "./DayList";
+
 import "components/Application.scss";
+import DayList from "./DayList";
+import InterviewerList from "./InterviewerList";
 import Appointment from "components/Appointment";
+import axios from 'axios';
 
 
-
-const appointments = {
-  "1": {
-    id: 1,
-    time: "12pm",
-  },
-  "2": {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer:{
-        id: 3,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  "3": {
-    id: 3,
-    time: "2pm",
-  },
-  "4": {
-    id: 4,
-    time: "3pm",
-    interview: {
-      student: "Archie Andrews",
-      interviewer:{
-        id: 4,
-        name: "Cohana Roy",
-        avatar: "https://i.imgur.com/FK8V841.jpg",
-      }
-    }
-  },
-  "5": {
-    id: 5,
-    time: "4pm",
+const appointments = [
+  {
   }
-};
+];
 
 
 
@@ -69,9 +37,27 @@ const days = [
 ];
 
 
+const appointment = appointments.map((appt) => {
+
+  return (
+      <Appointment key={appt.id} {...appt} />
+    )
+});
+
+
 
 export default function Application(props) {
-  const [day, setDay] = useState("Monday");
+  const [day, setDay] = useState([]);
+  const [dayData, setDayData] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:8001/api/days')
+      .then(res => {
+        setDayData(res.data)
+      })
+      .catch(err => console.log(err))
+  }, [])
+  
   return (
     <main className="layout">
       <section className="sidebar">
@@ -82,7 +68,7 @@ export default function Application(props) {
         />
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
-        <DayList days={days} day={day} setDay={setDay} />
+        <DayList days={dayData} day={day} setDay={setDay} />
         </nav>
         <img
           className="sidebar__lhl sidebar--centered"
