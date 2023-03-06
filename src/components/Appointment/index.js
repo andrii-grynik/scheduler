d2857331fbf5793e032af432045ca6791c70df14
andrii-grynik <andrii.grynik@gmail.com>
2023-03-06 14:59:50 -0700

    "Error Addition"

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -52,7 +52,7 @@ export default function Appointment(props) {
       props.bookInterview(props.id, interview)
         .then(() => transition(SHOW))
         .catch(() => transition(ERROR_SAVE, true))
-    } else {
+    } else { // allows error to populate for whole interview block 
       transition(ERROR_SAVE, false)
     } 
   }
